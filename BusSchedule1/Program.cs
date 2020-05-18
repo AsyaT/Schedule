ff8a792dd2b9ef3cbe1e7eb88b1d9663a46220e7
Asya Troyan <asya99@yandex.ru>
2020-05-18 14:41:36 +0500

    change algorithm

--- BusSchedule1/Program.cs
+++ BusSchedule1/Program.cs
@@ -64,15 +64,18 @@ namespace BusSchedule1
 
             // Shift preferences
 
-            for (byte i = 0; i < 14; i++)
+            for (byte i = 1; i <= 14; i++)
             {
-                for (byte j = 0; j < 11; j++)
+                for (byte j = 1; j <= 11; j++)
                 {
                     for (byte k = 0; k < 2; k++)
                     {
                         Dictionary<byte,byte> currentDriver = DriverPreferbaleShift.GetValueOrDefault(j);
 
-                        if (currentDriver!=null && currentDriver.GetValueOrDefault(i) == k && scheduleState[i, j, k] != 0)
+                        byte shiftNumber;
+                        currentDriver.TryGetValue(i, out shiftNumber);
+
+                        if (shiftNumber == k && scheduleState[i-1, j-1, k] != 0)
                         {
                             result += 3;
                         }
@@ -82,15 +85,16 @@ namespace BusSchedule1
 
             // Day-off preference
 
-            for (byte i = 0; i < 14; i++)
+            for (byte i = 1; i <= 14; i++)
             {
-                for (byte j = 0; j < 11; j++)
+                for (byte j = 1; j <= 11; j++)
                 {
                     for (byte k = 0; k < 2; k++)
                     {
                         byte[] prefDaysOff = DriverPreferableDaysOff.GetValueOrDefault(j);
+                        bool isTodayPrefDayoff = prefDaysOff != null && prefDaysOff.Contains(i);
 
-                        if (prefDaysOff!=null && prefDaysOff.Contains(i) && scheduleState[i, j, k] == 0)
+                        if (isTodayPrefDayoff && scheduleState[i-1, j-1, k] == 0)
                         {
                             result += 4;
                         }
@@ -237,31 +241,54 @@ namespace BusSchedule1
 
         static void FillInData()
         {
-            DriverAbilities.Add(1, new byte[] { 2 });
-            DriverAbilities.Add(2, new byte[] { 1 });
+            DriverAbilities.Add(1, new byte[] { 3 });
+            DriverAbilities.Add(2, new byte[] { 3 });
             DriverAbilities.Add(3, new byte[] { 1 });
-            DriverAbilities.Add(4, new byte[] { 2, 3 });
-            DriverAbilities.Add(5, new byte[] { 1, 2 });
-            DriverAbilities.Add(6, new byte[] { 2, 3 });
-            DriverAbilities.Add(7, new byte[] { 2 });
-            DriverAbilities.Add(8, new byte[] { 1, 3 });
-            DriverAbilities.Add(9, new byte[] { 2 });
-            DriverAbilities.Add(10, new byte[] { 3 });
-            DriverAbilities.Add(11, new byte[] { 1 });
+            DriverAbilities.Add(4, new byte[] { 1 });
+            DriverAbilities.Add(5, new byte[] { 1 });
+            DriverAbilities.Add(6, new byte[] { 1, 3 });
+            DriverAbilities.Add(7, new byte[] { 3 });
+            DriverAbilities.Add(8, new byte[] { 2 });
+            DriverAbilities.Add(9, new byte[] { 2, 3 });
+            DriverAbilities.Add(10, new byte[] { 1, 2 });
+            DriverAbilities.Add(11, new byte[] { 1, 2 });
 
-            DriverDaysOff.Add(1, new byte[] { 3, 4, 10, 11 });
-            DriverDaysOff.Add(2, new byte[] { 4, 5, 11, 12 });
-            // TODO: to finish
+            DriverDaysOff.Add(1, new byte[] { 5, 6, 12, 13 });
+            DriverDaysOff.Add(2, new byte[] { 6, 7, 13, 14 });
+            DriverDaysOff.Add(3, new byte[] { 1, 7, 8, 14 });
+            DriverDaysOff.Add(4, new byte[] { 1, 2, 8, 9, });
+            DriverDaysOff.Add(5, new byte[] { 2, 3, 9, 10 });
+            DriverDaysOff.Add(6, new byte[] { 3, 4, 10, 11 });
+            DriverDaysOff.Add(7, new byte[] { 4, 5, 11, 12 });
+            DriverDaysOff.Add(8, new byte[] { 5, 6, 12, 13 });
+            DriverDaysOff.Add(9, new byte[] { 6, 7, 13, 14 });
+            DriverDaysOff.Add(10, new byte[] { 1, 7, 8, 14 });
+            DriverDaysOff.Add(11, new byte[] { 1, 2, 8, 9 });
 
-            DriverPreferableDaysOff.Add(1, new byte[] { 8, 14 });
-            DriverPreferableDaysOff.Add(2, new byte[] { 7, 8 });
-            DriverPreferableDaysOff.Add(3, new byte[] { 1, 10 });
-            //TODO: to finish
+            DriverPreferableDaysOff.Add(1, new byte[] { 3, 8 });
+            DriverPreferableDaysOff.Add(2, new byte[] { 1, 9, 10 });
+            DriverPreferableDaysOff.Add(3, new byte[] { 3, 5, 10 });
+            DriverPreferableDaysOff.Add(4, new byte[] { 14 });
+            DriverPreferableDaysOff.Add(5, new byte[] { 6, 12 });
+            DriverPreferableDaysOff.Add(6, new byte[] { 7, 14 });
+            DriverPreferableDaysOff.Add(7, new byte[] { 1, 7, 8, 9 });
+            DriverPreferableDaysOff.Add(8, new byte[] { 1, 9, 10 });
+            DriverPreferableDaysOff.Add(9, new byte[] { 2, 3, 4 });
+            DriverPreferableDaysOff.Add(10, new byte[] { 3 });
+            DriverPreferableDaysOff.Add(11, new byte[] { 5, 6, 13 });
 
-
-            DriverPreferbaleShift.Add(1, new Dictionary<byte, byte>() { { 7, 1 }, { 9, 0 }, { 12, 0 }, { 13, 0 } });
-            DriverPreferbaleShift.Add(2, new Dictionary<byte, byte>() { { 1, 1 }, { 6, 0 }, { 9, 1 } });
-            //TODO : to finish
+            DriverPreferbaleShift.Add(1, new Dictionary<byte, byte>() { { 1, 1 }, { 10, 0 } });
+            DriverPreferbaleShift.Add(2, new Dictionary<byte, byte>() { { 4, 1 }, { 11, 0 } });
+            DriverPreferbaleShift.Add(3, new Dictionary<byte, byte>() { { 2,1 }, {9,1}, {11,0} });
+            DriverPreferbaleShift.Add(4, new Dictionary<byte, byte>() { { 4,0 },{5, 1} });
+            DriverPreferbaleShift.Add(5, new Dictionary<byte, byte>() { { 8,1 }, {11,1} });
+            DriverPreferbaleShift.Add(6, new Dictionary<byte, byte>() { { 6,1 },{8,1},{13,0} });
+            DriverPreferbaleShift.Add(7, new Dictionary<byte, byte>() { {13,0  } });
+            DriverPreferbaleShift.Add(8, new Dictionary<byte, byte>() { { 4,0 }, {8,0}, {14,1} });
+            DriverPreferbaleShift.Add(9, new Dictionary<byte, byte>() { {1,1  } });
+            DriverPreferbaleShift.Add(10, new Dictionary<byte, byte>() { {4,1  },{5,1}, {11,1} });
+            DriverPreferbaleShift.Add(11, new Dictionary<byte, byte>() { {3,0  },{7,0},{ 14,1} });
+ 
         }
     }
 }
